f5a674485de0736b44dc6a501da6650cdf2b8566
Heather Ward <heather@dnastack.com>
2023-08-15 17:50:42 -0400

    Add command line args to process script

--- docker/multiome/scripts/main/process.R
+++ docker/multiome/scripts/main/process.R
@@ -1,29 +1,42 @@
-working_dir <- '/data/CARD_singlecell/harmony-rna/'; setwd(working_dir)
+# Parse command-line arguments
+library('argparse')
+parser <- ArgumentParser(description='Normalize seurat objects')
+parser$add_argument('--working-dir', dest='working_dir', type='character', help='Working directory', default='/data/CARD_singlecell/harmony-rna/')
+parser$add_argument('--script-dir', dest='script_dir', type='character', help='Directory containing workflow scripts', default='scripts')
+parser$add_argument('--threads', dest='threads', type='integer', help='Number of threads to use for processing')
+parser$add_argument('--seurat-object', dest='seurat_object', type='character', help='Seurat object for a dataset')
+parser$add_argument('--output-seurat-object', dest='output_seurat_object', type='character', help='Output file to save Seurat object to')
+args <- parser$parse_args()
 
-source('scripts/main/load_packages.r')
+# Set working directory and load packages
+setwd(args$working_dir)
+source(paste0(args$script_dir, '/main/load_packages.r'))
 
-future::plan('multicore', workers=snakemake@threads)
+# Set variables from args or snakemake parameters
+threads <- if (is.null(args$threads)) snakemake@threads else args$threads
+seurat_object <- if (is.null(args$seurat_object)) snakemake@input[['seurat_object']] else args$seurat_object
+output_seurat_object <- if (is.null(args$output_seurat_object)) snakemake@output[['seurat_object']] else args$output_seurat_object
+
+# Main
+future::plan('multicore', workers=threads)
 options(future.globals.maxSize=ngbs * 1000 * 1024^2)
 
 assay <- 'RNA'
 
-object <- readRDS(snakemake@input[['seurat_object']])
-
+object <- readRDS(seurat_object)
 
 s.genes <- cc.genes$s.genes
 g2m.genes <- cc.genes$g2m.genes
 
-
 all.genes <- rownames(object)
 
-object <- object %>% 
+object <- object %>%
 
-    NormalizeData() %>% 
-    CleanVarGenes(nHVG=3000, use.sct=TRUE) %>% 
-    ScaleData(features=all.genes, verbose=FALSE) %>% 
-    CellCycleScoring(s.features=s.genes, g2m.features=g2m.genes) %>% 
+    NormalizeData() %>%
+    CleanVarGenes(nHVG=3000, use.sct=TRUE) %>%
+    ScaleData(features=all.genes, verbose=FALSE) %>%
+    CellCycleScoring(s.features=s.genes, g2m.features=g2m.genes) %>%
 
     DietSeurat(assays=assay)
 
-
-saveRDS(object, snakemake@output[['seurat_object']])
+saveRDS(object, output_seurat_object)
