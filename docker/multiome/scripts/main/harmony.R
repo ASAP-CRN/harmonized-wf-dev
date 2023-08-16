b78ece755b12fb0efd28941bd6d62e8b5f394379
Heather Ward <heather@dnastack.com>
2023-08-16 14:23:44 -0400

    Allow harmony script to be called using positional or keyword args

--- docker/multiome/scripts/main/harmony.R
+++ docker/multiome/scripts/main/harmony.R
@@ -1,18 +1,37 @@
-working_dir <- '/data/CARD_singlecell/harmony-rna/'; setwd(working_dir)
+# Parse command-line arguments
+library('argparse')
+parser <- ArgumentParser(description='Run harmony analysis')
+parser$add_argument('--working-dir', dest='working_dir', type='character', help='Working directory', default='/data/CARD_singlecell/harmony-rna/')
+parser$add_argument('--script-dir', dest='script_dir', type='character', help='Directory containing workflow scripts', default='scripts')
+parser$add_argument('--threads', dest='threads', type='integer', help='Number of threads to use for processing')
+parser$add_argument('--seurat-objects', dest='seurat_objects', type='character', nargs='+', help='Set of input seurat objects for datasets')
+parser$add_argument('--output-seurat-object', dest='output_seurat_object', type='character', help='Output file to save Seurat object to')
+# Accept positional arguments for compatibility with snakemake workflow
+parser$add_argument('positional_arguments', nargs='*', type='character', help='Optionally proivde arguments positionally. Format: `harmony.R seurat_objects output_seurat_object threads`. seurat_objects is a space-separated set of input seurat objects.')
+args <- parser$parse_args()
 
-source('scripts/main/load_packages.r')
-arguments <- commandArgs(trailingOnly=TRUE)
+## If positional arguments are used, ensure there are at least 3 (seurat_objects, output_seurat_object, threads)
+if (length(args$positional_arguments) > 0 && length(args$positional_arguments) < 3) {
+    cat("Unexpected number of positional arguments provided; positional arguments should be provided in the format:\n\tseurat_objectA seurat_objectB ... seurat_objectZ output_seurat_object threads\nArgs provided:\n\t")
+    cat(args$positional_arguments)
+    cat("\n")
+    stop()
+}
 
-future::plan('multicore', workers=as.numeric(tail(arguments, n=1)))
+# Set working directory and load packages
+setwd(args$working_dir)
+source(paste0(args$script_dir, '/main/load_packages.r'))
+
+# Set variables from args
+threads <- if (length(args$positional_arguments) > 0) as.numeric(tail(args$positional_arguments, n=1)) else args$threads
+seurat_objects <- if (length(args$positional_arguments) > 0) head(args$positional_arguments, -2) else args$seurat_objects
+output_seurat_object <- if (length(args$positional_arguments) > 0) tail(args$positional_arguments, n=2) else args$output_seurat_object
+
+# Main
+future::plan('multicore', workers=threads)
 options(future.globals.maxSize=ngbs * 1000 * 1024^2)
 
-
-input.files <- Filter(Negate(is.null), lapply(arguments, function(argument) {
-    if (argument %like% '_03.rds') argument
-}))
-
-
-object.list <- future.apply::future_lapply(input.files, readRDS)
+object.list <- future.apply::future_lapply(seurat_objects, readRDS)
 
 top.genes <- object.list %>% SelectIntegrationFeatures(nfeatures=5000)
 
@@ -22,15 +41,11 @@ message('Done.')
 
 batch <- 'sample'; noise <- c('percent.mt', 'percent.rb', 'nFeature_RNA', 'nCount_RNA', 'doublet_scores', 'G2M.Score', 'S.Score')
 
+object <- object %>%
 
-object <- object %>% 
+    ScaleData(vars.to.regress=noise, features=top.genes, verbose=FALSE) %>%
+    RunPCA(features=top.genes, npcs=50, verbose=FALSE) %>%
 
-            ScaleData(vars.to.regress=noise, features=top.genes, verbose=FALSE) %>% 
-            RunPCA(features=top.genes, npcs=50, verbose=FALSE) %>% 
-            
-            harmony::RunHarmony(reduction='pca', group.by.vars=batch, max.iter.harmony=50, reduction.save='harmony')
+    harmony::RunHarmony(reduction='pca', group.by.vars=batch, max.iter.harmony=50, reduction.save='harmony')
 
-
-saveRDS(object, tail(arguments, n=2)[1])
-
-
+saveRDS(object, output_seurat_object)[1])
