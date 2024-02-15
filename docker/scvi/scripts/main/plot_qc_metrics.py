4a2a8e499e6343a5c8d3e817c248c9fe86352947
Andy Henrie <arrrgo@gmail.com>
2024-02-15 14:42:04 -0800

    plot_qc_metric cleanup ✅

--- docker/scvi/scripts/main/plot_qc_metrics.py
+++ docker/scvi/scripts/main/plot_qc_metrics.py
@@ -1,34 +1,31 @@
 import argparse
-import os
-import sys
-import pandas as pc
-import scanpy as sc
-import anndata as ad
+import scanpy 
+import anndata
 
 
 parser = argparse.ArgumentParser(
     description='Call doublets'
 )
-parser.add_argument(
-    '--working-dir',
-    dest='working_dir',
-    type=str,
-    help='Working directory',
-    default='/data/CARD_singlecell/harmony-rna/'
-)
-parser.add_argument(
-    '--script-dir',
-    dest='script_dir',
-    type=str,
-    help='Directory containing workflow scripts',
-    default='scripts'
-)
-parser.add_argument(
-    '--threads',
-    dest='threads',
-    type=int,
-    help='Number of threads to use for processing'
-)
+# parser.add_argument(
+#     '--working-dir',
+#     dest='working_dir',
+#     type=str,
+#     help='Working directory',
+#     default='/data/CARD_singlecell/harmony-rna/'
+# )
+# parser.add_argument(
+#     '--script-dir',
+#     dest='script_dir',
+#     type=str,
+#     help='Directory containing workflow scripts',
+#     default='scripts'
+# )
+# parser.add_argument(
+#     '--threads',
+#     dest='threads',
+#     type=int,
+#     help='Number of threads to use for processing'
+# )
 parser.add_argument(
     '--adata-objects-fofn',
 	dest='adata_objects_fofn',
@@ -41,12 +38,7 @@ parser.add_argument(
 	type=str,
     help='Project name'
 )
-# parser.add_argument(
-#     '--output-metadata-file',
-# 	dest='output_metadata_file',
-# 	type=str,
-#     help='Output file to write metadata to'
-# )
+
 parser.add_argument(
     '--adata-output',
 	dest='adata_output',
@@ -56,14 +48,10 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-# os.chdir(args.working_dir)
-# sys.path.append('/opt/scripts/utility')
-# from helpers import minify_adata
 
-
-sc.settings.verbosity = 1
-sc.settings.figdir = 'plots/'
-sc.settings.set_figure_params(
+scanpy.settings.verbosity = 1
+scanpy.settings.figdir = 'plots/'
+scanpy.settings.set_figure_params(
     dpi=100,
     fontsize=10,
     dpi_save=300,
@@ -72,9 +60,9 @@ sc.settings.set_figure_params(
 ) # type: ignore
 
 metrics = ['n_genes_by_counts', 'total_counts', 'pct_counts_mt', 'pct_counts_rb', 'doublet_score']
-sc.settings.verbosity = 1
-sc.settings.figdir = 'plots/'
-sc.settings.set_figure_params(
+scanpy.settings.verbosity = 1
+scanpy.settings.figdir = 'plots/'
+scanpy.settings.set_figure_params(
     dpi=100,
     fontsize=10,
     dpi_save=300,
@@ -85,18 +73,22 @@ sc.settings.set_figure_params(
 adatas = {}
 top_genes = {}
 
+
+# TODO:  change how we get sample names / file names to aggregate?
+#  note that the sample id should be the official ASAP_samples 
 with open(args.adata_objects_fofn, 'r') as file:
     file_contents = file.read()
+
 # Remove empty elements in list, this will cause an error when reading in
 samples = list(filter(None, file_contents.split('\n')))
 
 for sample in samples:
-    raw = sc.read_h5ad(sample)
+    raw = scanpy.read_h5ad(sample)
     # code below if memory issues with concatenating all the ge
     # adata = raw.copy()
-    # sc.pp.normalize_total(adata, target_sum=1e4)
-    # sc.pp.log1p(adata)
-    # sc.pp.highly_variable_genes(adata, flavor='seurat', n_top_genes=8000)
+    # scanpy.pp.normalize_total(adata, target_sum=1e4)
+    # scanpy.pp.log1p(adata)
+    # scanpy.pp.highly_variable_genes(adata, flavor='seurat', n_top_genes=8000)
 
     # # ranked_genes = adata.var[adata.var.highly_variable].dispersions_norm.argsort().to_dict()   
     # for k,v in ranked_genes.items():
@@ -110,8 +102,7 @@ for sample in samples:
     adatas[sample_name] = raw
 
 # we could subset to the top_genes here before concat if we have memory issues (e.g. whole dataset harmonization.)
-
-adata = ad.concat(
+adata = anndata.concat(
             merge='same',
             uns_merge='same',
             index_unique='_',
@@ -120,14 +111,10 @@ adata = ad.concat(
 
 
 for metric in metrics: # type: ignore
-    sc.pl.violin(adata, keys=metric, size=0, save=''.join('_' + metric))
-
+    scanpy.pl.violin(adata, keys=metric, size=0, save=''.join('_' + metric))
 
 # top_genes = pd.DataFrame(index=top_genes.keys(), columns=['rank'], data=top_genes.values())
-
 #TODO: export top_genes? and plots
 
 # export concatenated data.
-# TODO - write_h5ad option compression='gzip' is giving an error
-#adata.write_h5ad(filename=args.adata_output, compression='gzip')
 adata.write_h5ad(filename=args.adata_output)
