32de6c2763cb487600914dd7ea6407f1556f1093
Andy Henrie <arrrgo@gmail.com>
2024-02-15 13:49:10 -0800

    process ✅

--- docker/scvi/scripts/main/process.py
+++ docker/scvi/scripts/main/process.py
@@ -1,6 +1,4 @@
 import argparse
-import os
-import pandas as pd
 import scanpy
 import sys
 sys.path.append('/opt/scripts/utility')
@@ -10,13 +8,7 @@ from helpers import score_cell_cycle
 parser = argparse.ArgumentParser(
     description='Normalize seurat objects'
 )
-parser.add_argument(
-	'--working-dir',
-	dest='working_dir',
-	type=str,
-    help='Working directory',
-	default='/data/CARD_singlecell/harmony-rna/'
-)
+
 parser.add_argument(
 	'--adata-input',
 	dest='adata_input',
@@ -52,6 +44,7 @@ adata.layers['counts'] = adata.X.copy() # type: ignore
 scanpy.pp.normalize_total(adata, target_sum=1e4)
 scanpy.pp.log1p(adata)
 
+# get cell cycle scores
 score_cell_cycle(adata, organism="human")
 
 # sc.tl.score_genes_cell_cycle(adata, s_genes=s_genes, g2m_genes=g2m_genes)
@@ -64,6 +57,5 @@ scanpy.pp.highly_variable_genes(
     n_top_genes=args.n_top_genes
 )
 
-# TODO - write_h5ad option compression='gzip' is giving an error
-#adata.write_h5ad(filename=args.adata_output, compression='gzip')
+#  write_h5ad option compression='gzip' is giving an error
 adata.write_h5ad(filename=args.adata_output)
